c54bcfd528d30d857af55116ca860c6eba120ec0
Sofia <a.sofia00@hotmail.com>
2016-11-28 21:45:45 +0000

    modifica permissões de comboio

--- scripts/scripts_criacao/CreateComboio.sql
+++ scripts/scripts_criacao/CreateComboio.sql
@@ -1,10 +1,10 @@
 CREATE USER 'agencia.comboio'@'localhost'
 IDENTIFIED BY 'Agencia';
-/*Concedem-se os provilégios de inserção, atualização e seleção:*/
-GRANT INSERT,UPDATE,SELECT
+/*Concedem-se os provilégios de inserção e seleção:*/
+GRANT INSERT,SELECT
 ON Agencia.Comboio
 TO 'agencia.comboio'@'localhost';
-/*E retira-se o privilégio de remoção na tabela Cliente :*/
-REVOKE DELETE
+/*E retiram-se os privilégios de atualização e remoção na tabela Cliente :*/
+REVOKE UPDATE,DELETE
 ON Agencia.Comboio
 FROM 'agencia.comboio'@'localhost';
