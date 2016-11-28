0c90c86aace8bd3d977b30f1449fcab4b5627fc3
Sofia <a.sofia00@hotmail.com>
2016-11-28 01:32:17 +0000

    tamanho bd

--- scripts/scripts/queriesTamanhoBD.sql
+++ scripts/scripts/queriesTamanhoBD.sql
@@ -1,9 +1,11 @@
-CALL sp_localizacao_exemplares('Decision in Normandy');
+CALL Agencia('Decision in Normandy');
 
-SELECT table_schema 'DB Name', SUM(data_length + index_length) / 1024 / 1024 'DB Size in MB'
+SELECT table_schema 'Agencia', SUM(data_length + index_length) / 1024 / 1024 'DB Size in MB'
 	FROM information_schema.tables
-	WHERE table_schema = 'bgum';
+	WHERE table_schema = 'Agencia';
 
 SELECT TABLE_NAME, table_rows, data_length / 1024 'Dados (KB)', index_length / 1024 'Indices (KB)', 
 round(((data_length + index_length) / 1024 / 1024),5) 'Size in MB'
-	FROM information_schema.TABLES WHERE table_schema = 'bgum';
+	FROM information_schema.TABLES WHERE table_schema = 'Agencia';
+
+
